576ebe033a411c2f2a98a2cab44bd5f0c693cce9
KJ Kim <gencode.me@gmail.com>
2020-02-15 12:08:11 +0900

    release 0.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(f):
 
 setup(
     name='django-traffic-monitor',
-    version=0.5,
+    version=0.7,
     url='https://github.com/genonfire/django-traffic-monitor',
     license='MIT',
     description='a Django application that eases to monitor server traffic.',
