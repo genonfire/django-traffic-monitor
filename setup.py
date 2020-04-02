7a6d10f111dbdc5d55f45817e9e10ee6ae2f2576
KJ Kim <gencode.me@gmail.com>
2020-04-02 11:27:26 +0900

    release 1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(f):
 
 setup(
     name='django-traffic-monitor',
-    version=1.0,
+    version=1.1,
     url='https://github.com/genonfire/django-traffic-monitor',
     license='MIT',
     description='a Django application that eases to monitor server traffic.',
--- traffic_monitor/urls.py
+++ traffic_monitor/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'traffic_monitor'
+
 urlpatterns = [
     url(
         r'^$',
